eabb506a7571a7fb3820a8afb59aaea0b8346906
Tamish123 <tamishchaudhary782@gmail.com>
2025-08-21 13:25:45 +0530

    Update count=1.py

--- count=1.py
+++ count=1.py
@@ -1,15 +1,16 @@
-# count=1
-# while(count<12):
-#     print("tamish")
-#     count=count+1
+count=1
+ while(count<12):
+     print("tamish")
+     count=count+1
 
 
-# n=int(input("enter the value:"))
-# i=1
-# while(1<=n):
-#     if(n%i==0):
-#         print(i)
-#     i=i+1
+ n=int(input("enter the value:"))
+ i=1
+ while(1<=n):
+     if(n%i==0):
+         print(i)
+     i=i+1
+
 
 
 
